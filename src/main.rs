1551d5cd5bebc8d39dcf39b08b52b211cb495d1d
jonas <jonas.schwab@web.de>
2021-12-04 11:00:02 +0100

    initial commit

--- src/main.rs
+++ src/main.rs
@@ -69,42 +69,121 @@ fn main() {
         println!("{}", position.0 * position.1);
     }
 
-
     println!("==== Day 3 ====");
     {
-        let commands = include_str!("../input/day3.txt").lines()
+        let lines = include_str!("../input/day3.txt").lines()
             .map(|line| line.split(" ").collect::<Vec<&str>>()).collect::<Vec<_>>();
 
         println!("--- Solution Part One ---");
-        let mut position:(i32,i32) = (0, 0);
-        for command in &commands {
-            if let Ok(delta) = command[1].parse::<i32>() {
-                match command[0] {
-                    "forward" => position.0 += delta,
-                    "down" => position.1 += delta,
-                    "up" => position.1 -= delta,
-                    _ => panic!(),
+        let mut b = vec!(0,0,0,0,0,0,0,0,0,0,0,0);
+        for line in &lines {
+            for (i,c) in line[0].chars().enumerate(){
+                if c=='1' {
+                    b[i] += 1;
                 }
             }
         }
-        println!("{}", position.0 * position.1);
+        let mut gamma = 0;
+        let mut epsilon = 0;
+
+        for i in 0..12 {
+            if (2 * b[11-i]) >= lines.len(){
+                gamma += 1<<i;
+            }else{
+                epsilon += 1<<i;
+            }
+        }
+        println!("{}", gamma * epsilon);
 
         println!("--- Solution Part Two ---");
-        let mut aim:i32 = 0;
-        position = (0, 0);
-        for command in &commands {
-            if let Ok(delta) = command[1].parse::<i32>() {
-                match command[0] {
-                    "forward" => {
-                        position.0 += delta;
-                        position.1 += delta * aim
-                    },
-                    "down" => aim += delta,
-                    "up" => aim -= delta,
-                    _ => panic!(),
+
+        let mut char_vec_g:Vec<_> =lines.iter().map(|l| l[0]).collect();
+        let mut char_vec_e=char_vec_g.clone();
+        for i in 0..char_vec_g[0].len() {
+            let filter_g = char_vec_g.iter()
+                .fold(0, |n, s| if s.chars().nth(i) == Some('1') { n + 1 } else { n }) * 2
+                >= char_vec_g.len();
+            char_vec_g = char_vec_g.iter().cloned()
+                .filter(|s| (s.chars().nth(i) == Some('1')) == filter_g).collect::<Vec<_>>();
+
+            if char_vec_g.len() == 1 {
+                break;
+            }
+        }
+        for i in 0..char_vec_e[0].len() {
+            let filter_e = char_vec_e.iter()
+                .fold(0, |n, s| if s.chars().nth(i) == Some('1') { n + 1 } else { n }) * 2
+                < char_vec_e.len();
+            char_vec_e = char_vec_e.iter().cloned()
+                .filter(|s| (s.chars().nth(i) == Some('1')) == filter_e).collect::<Vec<_>>();
+
+            if char_vec_e.len() == 1 {
+                break;
+            }
+        }
+        if let Ok(ls1) = i32::from_str_radix(char_vec_g[0], 2) {
+            if let Ok(ls2) = i32::from_str_radix(char_vec_e[0], 2) {
+                println!("{:?}", ls1 * ls2);
+            }
+        }
+    }
+
+    println!("==== Day 4 ====");
+    {
+        let lines = include_str!("../input/day3.txt").lines()
+            .map(|line| line.split(" ").collect::<Vec<&str>>()).collect::<Vec<_>>();
+
+        println!("--- Solution Part One ---");
+        let mut b = vec!(0,0,0,0,0,0,0,0,0,0,0,0);
+        for line in &lines {
+            for (i,c) in line[0].chars().enumerate(){
+                if c=='1' {
+                    b[i] += 1;
                 }
             }
         }
-        println!("{}", position.0 * position.1);
+        let mut gamma = 0;
+        let mut epsilon = 0;
+
+        for i in 0..12 {
+            if (2 * b[11-i]) >= lines.len(){
+                gamma += 1<<i;
+            }else{
+                epsilon += 1<<i;
+            }
+        }
+        println!("{}", gamma * epsilon);
+
+        println!("--- Solution Part Two ---");
+
+        let mut char_vec_g:Vec<_> =lines.iter().map(|l| l[0]).collect();
+        let mut char_vec_e=char_vec_g.clone();
+        for i in 0..char_vec_g[0].len() {
+            let filter_g = char_vec_g.iter()
+                .fold(0, |n, s| if s.chars().nth(i) == Some('1') { n + 1 } else { n }) * 2
+                >= char_vec_g.len();
+            char_vec_g = char_vec_g.iter().cloned()
+                .filter(|s| (s.chars().nth(i) == Some('1')) == filter_g).collect::<Vec<_>>();
+
+            if char_vec_g.len() == 1 {
+                break;
+            }
+        }
+        for i in 0..char_vec_e[0].len() {
+            let filter_e = char_vec_e.iter()
+                .fold(0, |n, s| if s.chars().nth(i) == Some('1') { n + 1 } else { n }) * 2
+                < char_vec_e.len();
+            char_vec_e = char_vec_e.iter().cloned()
+                .filter(|s| (s.chars().nth(i) == Some('1')) == filter_e).collect::<Vec<_>>();
+
+            if char_vec_e.len() == 1 {
+                break;
+            }
+        }
+        if let Ok(ls1) = i32::from_str_radix(char_vec_g[0], 2) {
+            if let Ok(ls2) = i32::from_str_radix(char_vec_e[0], 2) {
+                println!("{:?}", ls1 * ls2);
+            }
+        }
     }
 }
